3005540bd702e520bdc8580b374d3abb5415090b
justanotherjoke <david.facsko@gmail.com>
2020-07-20 00:08:44 +0200

    rails project in docker with mysql

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = '1675d30b1d585b2ab5f0ddf12504eae6f9a1cfa99423c0191e1e16acb039a632c390057f27f45c1927ebc9ee28f675b5d8c99d54cd2667260e0ca2646684841e'
